16d85e33ee1bfa2d08fa3ed8e711273311db8789
Benjamin Ryder wismath <benjaminryderwismath@MacBook-Air-de-Benjamin.local>
2026-05-06 10:43:49 -0300

    feat: add root endpoint

--- src/app.js
+++ src/app.js
@@ -4,17 +4,32 @@ const app = express();
 
 app.use(express.json());
 
+// 🔥 Ruta raíz (IMPORTANTE)
+app.get("/", (req, res) => {
+res.json({
+    message: "Investment Portfolio API running 🚀",
+    endpoints: {
+        auth: "/auth",
+        portafolios: "/portafolios",
+        activos: "/activos",
+        transacciones: "/portafolios/:id/transacciones",
+        alertas: "/alertas"
+    }
+    });
+});
+
 app.use("/auth", require("./routes/auth.routes"));
 app.use("/portafolios", require("./routes/portafolios.routes"));
 app.use("/activos", require("./routes/activos.routes"));
 app.use("/portafolios/:id/transacciones", require("./routes/transacciones.routes"));
-app.use("/alertas", require ("./routes/alertas.routes"));
+app.use("/alertas", require("./routes/alertas.routes"));
+
 
 app.use((err, req, res, next) => {
     const statusCode = err.statusCode || 500;
     res.status(statusCode).json({
         status: "error",
-        message: err.message || `Error interno en el servidor`
+        message: err.message || "Error interno en el servidor"
     });
 });
 
